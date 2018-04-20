d1a5ba261a33499268ce77c52f20b2c461f0dbec
sadeghb97 <sadeghb97@gmail.com>
2018-04-20 16:45:24 +0430

    Intial Git - Uni - Term 4

---
+++ build/CMakeFiles/rodcutproblem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rodcutproblem.dir/main.cpp.o"
+  "rodcutproblem.pdb"
+  "rodcutproblem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rodcutproblem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
